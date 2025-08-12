3113fcb2dcdf11376a3aff380360e82f220b63bf
kochmas <118772386+kochmas@users.noreply.github.com>
2025-08-12 10:27:15 +0200

    chore: set capacitor webDir

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'auditory.trainer',
+  appName: 'Auditory Trainer',
+  webDir: 'listen-up',
+  bundledWebRuntime: false,
+};
+
+export default config;
